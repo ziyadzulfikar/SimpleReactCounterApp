a89f4d37d57a292cfe6af0fddeaf2f0fb415a128
Ziyad Zulfikar <56788667+ziyadzulfikar@users.noreply.github.com>
2020-09-16 13:57:05 +0530

    Updates

---
+++ precache-manifest.8c10df9318f07f277b69d227fc0cf9f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b667400d99daaf4010172e767d89eb87",
+    "url": "/SimpleReactCounterApp/index.html"
+  },
+  {
+    "revision": "dc002d18a49d2bf8a892",
+    "url": "/SimpleReactCounterApp/static/css/main.2ba6874d.chunk.css"
+  },
+  {
+    "revision": "8961004a6b589eb17a4c",
+    "url": "/SimpleReactCounterApp/static/js/2.e1a0f1bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SimpleReactCounterApp/static/js/2.e1a0f1bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc002d18a49d2bf8a892",
+    "url": "/SimpleReactCounterApp/static/js/main.88cf99ca.chunk.js"
+  },
+  {
+    "revision": "71de0268e88c20594c6e",
+    "url": "/SimpleReactCounterApp/static/js/runtime-main.c1f63711.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/SimpleReactCounterApp/static/media/logo.5d5d9eef.svg"
+  }
+]);
